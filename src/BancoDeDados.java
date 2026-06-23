6d746e5703be59edb728439d60f66123aae2487c
JOAO PEDRO NAVARRO OKITA <joao.okita07@localhost.localdomain>
2026-06-23 17:14:34 -0300

    Implementação dos métodos new, get, delete (all)

--- src/BancoDeDados.java
+++ src/BancoDeDados.java
@@ -1,3 +1,39 @@
-public class Receptor {
-    
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Iterator;
+
+public class BancoDeDados {
+    private Map<integer, Pessoa> database = new HashMap<>();
+    public BancoDeDados() {}
+
+    public Object newPessoa(int id, string Nome) {
+        if (database.get(id) == null) {
+            database.put(id, new Pessoa(id, nome));
+        }
+    }
+
+    public Object deletePessoa(int id) {
+        if (database.get(id) != null) {
+            database.remove(id);
+        }
+    }
+
+    public Object deleteAll() {
+        database.clear();
+    }
+
+    public Object getPessoa(int id) {
+        return database.get(id);
+    }
+
+    public Object getAll() {
+        List<Pessoa> lista = new List<Pessoa>();
+
+        for (Pessoa p : database.values()) {
+            lista.add(p);
+        }
+
+        return lista;
+    }
 }
